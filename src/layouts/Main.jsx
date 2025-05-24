27386c609f933f4d019814a2d7f2a0ab002276ca
Louis2324 <miguellouis2023@gmail.com>
2025-05-24 09:46:44 +0200

    Added a folder to hold all parent layouts for routing

---
+++ src/layouts/Main.jsx
@@ -0,0 +1,30 @@
+//rrd imports
+import { Outlet, useLoaderData } from "react-router-dom";
+
+//helpers
+import { fetchData } from "../helpers";
+
+// loader function
+export function mainLoader() {
+  const userName = fetchData("userName");
+  return { userName };
+}
+
+//assets
+import wave from "../assets/wave.svg";
+import Nav from "../components/Nav";
+
+const Main = () => {
+  const { userName } = useLoaderData();
+  return (
+    <div className="layout">
+      <Nav userName={userName} />
+      <main>
+        <Outlet />
+      </main>
+      <img src={wave} alt="" />
+    </div>
+  );
+};
+
+export default Main;
